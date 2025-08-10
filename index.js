bb274351c41abd810e4d0de976848a05af336410
Pete ang Jaaa <sakayanagi101@gmail.com>
2025-08-11 04:10:07 +0700

    finish main page

--- index.js
+++ index.js
@@ -2,9 +2,25 @@ import express from "express";
 
 const app = express();
 const port = 3000;
-app.use(express.static("public"))
-app.use(express.urlencoded({ extended: true}))
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+var account = [
+  {
+    username: "0000",
+    amount: 1000000,
+  },
+];
+var lastPerson = 0;
+app.get("/", (req, res) => {
+  const newAccount = {
+    username: lastPerson,
+    amount: 1000000,
+  };
+  res.render("index.ejs", {
+    content: account[lastPerson],
+  });
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-})
+  console.log(`Server is running on http://localhost:${port}`);
+});
