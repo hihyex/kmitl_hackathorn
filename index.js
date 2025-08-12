fca3d58118f64037b839e6c99a95ef9c7c50e2b5
Pete ang Jaaa <sakayanagi101@gmail.com>
2025-08-12 22:54:32 +0700

    add port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from "express";
 import QRCode from "qrcode";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
