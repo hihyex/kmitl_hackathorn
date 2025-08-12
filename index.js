337d0cbc18842d4c33f7b13aaccd134f666bb95d
Pete ang Jaaa <sakayanagi101@gmail.com>
2025-08-12 23:12:19 +0700

    set ejs

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
-
+app.set("view engine", "ejs"); 
 var account = [];
 var whiteList = [];
 var lastId = 1;
